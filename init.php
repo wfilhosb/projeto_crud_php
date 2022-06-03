d0e420f94088be4e9f44e91b888370386bc712c1
Washington <wfilhosb@hotmail.com>
2022-06-02 21:27:09 -0300

    Teste com a nova senha

--- init.php
+++ init.php
@@ -2,6 +2,6 @@
 date_default_timezone_set('America/Sao_Paulo');
 define('BD_SERVIDOR','localhost');
 define('BD_USUARIO','root');
-define('BD_SENHA','');
+define('BD_SENHA','123456');
 define('BD_BANCO','crudphp');
 ?>
